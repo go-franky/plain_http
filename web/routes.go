b2af54e01c2d555752e3c2493d2301a82ec45a07
Franky W <frankywahl@users.noreply.github.com>
2019-09-11 19:17:32 -0700

    Make 404 happen

--- web/routes.go
+++ web/routes.go
@@ -1,10 +1,21 @@
 package web
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (s *Server) routes() {
-	router := http.DefaultServeMux
-	router.HandleFunc("/", s.log(s.helloWorld()))
-	router.HandleFunc("/health", s.log(s.health()))
+	router := http.NewServeMux()
+	router.HandleFunc("/", s.log(s.rootHandler()))
 	s.Handler = router
+}
+
+func (s *Server) rootHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		s.health()(w, r)
+	}
 }
