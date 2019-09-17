f57045df8227c20fc2db59fd4b11ddaa6cd60b0b
Franky W <frankywahl@users.noreply.github.com>
2019-09-16 19:14:33 -0700

    Use built in Time marshaler

--- api/timestamp.go
+++ api/timestamp.go
@@ -1,9 +1,6 @@
 package api
 
 import (
-	"errors"
-	"io"
-	"strconv"
 	"time"
 
 	"github.com/99designs/gqlgen/graphql"
@@ -11,15 +8,17 @@ import (
 
 // MarshalTimestamp redefines the scalar timestamp to be formatted properly
 func MarshalTimestamp(t time.Time) graphql.Marshaler {
-	return graphql.WriterFunc(func(w io.Writer) {
-		io.WriteString(w, strconv.Quote(t.UTC().Format(time.RFC3339)))
-	})
+	return graphql.MarshalTime(t)
+	// return graphql.WriterFunc(func(w io.Writer) {
+	// io.WriteString(w, strconv.Quote(t.UTC().Format(time.RFC3339)))
+	// })
 }
 
 // UnmarshalTimestamp does the same thing for the unmarshaler
 func UnmarshalTimestamp(v interface{}) (time.Time, error) {
-	if tmpStr, ok := v.(string); ok {
-		return time.Parse(time.RFC3339, tmpStr)
-	}
-	return time.Time{}, errors.New("timestamp error")
+	// if tmpStr, ok := v.(string); ok {
+	// return time.Parse(time.RFC3339, tmpStr)
+	// }
+	// return time.Time{}, errors.New("time should be RFC3339 formatted string")
+	return graphql.UnmarshalTime(v)
 }
